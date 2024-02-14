dfc8cc060944496c43d8ff3eb75c8687b5c8b0e5
priyanshu1831 <kaushik.priyanshu22@gmail.com>
2024-02-14 14:08:13 +0530

    initial commit

--- rpc/postgres-version.tsx
+++ rpc/postgres-version.tsx
@@ -1,5 +1,5 @@
 export default function postgresVersion() {
-  const [{ version }] = sql`SELECT version()`; // no `await` needed!
+  const [{ version }] = sql`SELECT version()`;
   return (
     <h1>
       Hello from <em>inside</em> Postgres: {version}
